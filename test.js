64832b9be881b5a7f7f60d3f8ed55ae50ccb4d16
Katy DeCorah <katydecorah@gmail.com>
2023-12-06 19:15:40 -0500

    Update dependencies (#13)
    * Update dependencies
    
    * Update workflow

--- test.js
+++ test.js
@@ -1,17 +1,13 @@
-'use strict';
-
-const remark = require('remark');
-const dedent = require('dedent');
-const plugin = require('.');
+import { remark } from "remark";
+import dedent from "dedent";
+import plugin from ".";
 
 const processMarkdown = (md, opts) => {
-  return remark()
-    .use(plugin, opts)
-    .process(md);
+  return remark().use(plugin, opts).process(md);
 };
 
-describe('remark-lint-haiku', () => {
-  test('works', () => {
+describe("remark-lint-haiku", () => {
+  test("works", () => {
     const lint = processMarkdown(dedent`
 # Title
 
@@ -21,12 +17,12 @@ I tried to cut from fabric
 I cut a rhombus
 \`\`\`
     `);
-    return lint.then(vFile => {
+    return lint.then((vFile) => {
       expect(vFile.messages.length).toBe(0);
     });
   });
 
-  test('not three lines', () => {
+  test("not three lines", () => {
     const lint = processMarkdown(dedent`
 # Title
 
@@ -35,13 +31,15 @@ Parallelogram
 I tried to cut from fabric
 \`\`\`
     `);
-    return lint.then(vFile => {
+    return lint.then((vFile) => {
       expect(vFile.messages.length).toBe(1);
-      expect(vFile.messages[0].message).toBe('Haiku has 2 lines, it should have 3 lines.');
+      expect(vFile.messages[0].message).toBe(
+        "Haiku has 2 lines, it should have 3 lines."
+      );
     });
   });
 
-  test('too few syllables', () => {
+  test("too few syllables", () => {
     const lint = processMarkdown(dedent`
 # Title
 
@@ -51,13 +49,15 @@ I tried to cut
 I cut a rhombus
 \`\`\`
     `);
-    return lint.then(vFile => {
+    return lint.then((vFile) => {
       expect(vFile.messages.length).toBe(1);
-      expect(vFile.messages[0].message).toBe('"I tried to cut" (4), it should have 7 syllables.');
+      expect(vFile.messages[0].message).toBe(
+        '"I tried to cut" (4), it should have 7 syllables.'
+      );
     });
   });
 
-  test('too many syllables', () => {
+  test("too many syllables", () => {
     const lint = processMarkdown(dedent`
 # Title
 
@@ -67,9 +67,11 @@ I tried to cut from my fabric
 I cut a rhombus
 \`\`\`
     `);
-    return lint.then(vFile => {
+    return lint.then((vFile) => {
       expect(vFile.messages.length).toBe(1);
-      expect(vFile.messages[0].message).toBe('"I tried to cut from my fabric" (8), it should have 7 syllables.');
+      expect(vFile.messages[0].message).toBe(
+        '"I tried to cut from my fabric" (8), it should have 7 syllables.'
+      );
     });
   });
 });
